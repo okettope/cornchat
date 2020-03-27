37b7e7525f29206c91ef6f9fdb8db347f566ebd7
cornelius <oketonaayoade@gmail.com>
2020-03-27 21:01:43 +0100

    first commit

---
+++ src/components/DevInfo/DevInfo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import '../DevInfo/DevInfo.css'
+
+function DevInfo() {
+    return (
+        <div>
+        <p className="devtag">Corn Chat <br/>Built with Node.js, React.js and Socket.io
+         <br/> By <a href="https://ayoadeoketona.netlify.com" target="_blank" rel="noopener noreferrer" className="devname"
+         >Corn Design</a></p>
+            
+        </div>
+    )
+}
+
+export default DevInfo
---
+++ src/components/Join/Join.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import DevInfo from '../DevInfo/DevInfo'
+
+import './Join.css'
+
+const Join = () => {
+
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+    return(
+        <div className="joinOuterContainer">
+            <div className="joinInnerContainer">
+                <h1 className="heading">Join</h1>
+                <div><input placeholder="Username" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} required /></div>
+                <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} maxLength="20"   required/></div>
+                <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                    <button className="button "  type="submit">Sign in </button>
+                </Link>
+
+                <br/><br/>
+                <DevInfo/>
+            </div> 
+
+        </div>
+        
+    );
+}
+
+export default Join;
